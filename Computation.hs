74fe5bd872894d35115314d45b2f3759341e0ded
michaelt <what_is_it_to_do_anything@yahoo.com>
2016-01-03 20:14:18 -0500

    free-vl

--- Computation.hs
+++ Computation.hs
@@ -1,10 +1,10 @@
-{-# LANGUAGE FlexibleContexts, RankNTypes #-}
+{-# LANGUAGE FlexibleContexts, RankNTypes, FlexibleInstances , DataKinds  #-}
 module Computation where
 
 import Base
 import Control.Monad
 import qualified Control.Monad.State.Strict as MTL
-
+import Control.Monad.Free.VanLaarhovenE
 computation
   :: (Monad m, MonadFree F m)
   => Int
@@ -12,9 +12,20 @@ computation
 computation n = forM_ [1..n] $ \_ -> do
   s <- get
   put $! s + 1
+{-#INLINABLE computation #-}
 
-mtlComputation :: Int -> MTL.State Int ()
-mtlComputation n = forM_ [1..n] $ \_ -> do
+mtlComputationIO ::  Int -> MTL.StateT Int IO ()
+mtlComputationIO n = forM_ [1..n] $ \_ -> do
+  s <- MTL.get
+  MTL.put $! s + 1  
+ 
+mtlComputationId:: Int -> MTL.State Int ()
+mtlComputationId n = forM_ [1..n] $ \_ -> do
+  s <- MTL.get
+  MTL.put $! s + 1
+
+mtlComputationGeneral:: Monad m => Int -> MTL.StateT Int m ()
+mtlComputationGeneral n = forM_ [1..n] $ \_ -> do
   s <- MTL.get
   MTL.put $! s + 1
 
@@ -30,7 +41,7 @@ computation2 n =
       s <- get
       put $! s + 1
 
-mtlComputation2 :: Int -> MTL.State Int ()
+mtlComputation2 :: Monad m => Int -> MTL.StateT Int m ()
 mtlComputation2 n =
   if n == 0
     then return ()
@@ -38,3 +49,31 @@ mtlComputation2 n =
       mtlComputation2 (n-1)
       s <- MTL.get
       MTL.put $! s + 1
+
+
+data State s m = State{getState :: m s, putState :: s -> m ()}
+
+get_ :: HasEffect effects (State s) => Free effects s
+get_  = liftF getState
+
+put_ :: HasEffect effects (State s) => s -> Free effects ()
+put_ s = liftF (\st -> putState st s)
+
+vlComputation
+    :: (HasEffect effects (State Int)) =>
+       Int -> Free effects ()
+
+vlComputation n = forM_ [1..n] $ \_ -> do
+    s <- get_
+    put_ $! s + (1::Int)
+
+    
+myState :: State s (MTL.State s)
+myState = State {getState = MTL.get, putState = MTL.put}
+
+stateInterp = myState .:. EmptyE
+
+vl  :: Free '[State Int] a -> MTL.State Int a
+vl = iterM stateInterp
+
+
--- Main.hs
+++ Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleContexts, RankNTypes #-}
+{-# LANGUAGE FlexibleContexts, RankNTypes#-}
 module Main (main) where
 
 import Base
@@ -10,10 +10,10 @@ import qualified NoRemorse
 import qualified Freer
 import Control.Monad
 import qualified Control.Monad.State.Strict as MTL
-
-import Criterion (bench, nf, bgroup, Benchmark)
+import Data.Functor.Identity
+import Criterion (bench, nf, nfIO, bgroup, Benchmark)
 import Criterion.Main (defaultMain)
-
+import qualified Control.Monad.Free.VanLaarhovenE as VL
 
 n = 50
 
@@ -23,17 +23,22 @@ benchmarks
   -> Int
   -> [Benchmark]
 benchmarks computation mtlComputation n =
-  [ bench "Free" $ nf (flip Free.run 0 . computation) n
+  [ 
+  bench "Free" $ nf (flip Free.run 0 . computation) n
   , bench "Free/lazy" $ nf (flip Free.runLazily 0 . computation) n
   , bench "Chruch" $ nf (flip Church.run 0 . computation) n
   , bench "Codensity" $ nf (flip Codensity.run 0 . computation) n
   , bench "NoRemorse" $ nf (flip NoRemorse.run 0 . computation) n
   , bench "Freer" $ nf (flip Freer.run 0 . computation) n
-  , bench "MTL" $ nf (flip MTL.runState 0 . mtlComputation) n
+  , bench "MTLGeneral" $ nf (flip MTL.runState 0 . mtlComputationGeneral) n
+  , bench "MTLIdentity" $ nf (flip MTL.runState 0 . mtlComputationId) n
+  , bench "MTLIO" $ nfIO (MTL.runStateT (mtlComputationIO n) 0) 
+  , bench "VL" $ nf (flip MTL.runState 0 . vl . vlComputation) n
+
   ]
 
 main :: IO ()
 main = defaultMain
-  [ bgroup "Right-assoc" $ benchmarks computation mtlComputation n
+  [ bgroup "Right-assoc" $ benchmarks computation mtlComputationId n
   , bgroup "Left-assoc" $ benchmarks computation2 mtlComputation2 n
   ]
